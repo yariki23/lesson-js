ad6cc1f118669d8da18fcf8700ad86ce1688ebf4
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-04-20 08:54:55 +0300

    new

--- DOM/search-elem-on-page/index.js
+++ DOM/search-elem-on-page/index.js
@@ -1,14 +1,14 @@
 "use strict";
 
-function getTitleElement() {
+export function getTitleElement() {
   const titleElem = document.querySelector(".title");
   console.dir(titleElem);
   return titleElem;
 }
-getTitleElement();
-function getInputElement() {
+// getTitleElement();
+export function getInputElement() {
   const inputElem = document.querySelector('[type="text"]');
   console.log(inputElem);
   return inputElem;
 }
-getInputElement();
+// getInputElement();
